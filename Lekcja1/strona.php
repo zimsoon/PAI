3731e31465f5daaedb4db007231e71e4b6e7ce94
zimsoon <kaamlos@wp.pl>
2022-09-13 11:03:17 +0200

    Komentarz

---
+++ Lekcja1/strona.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+	 <meta charset="utf-8">
+     <head>
+		<title>NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN</title>
+     </head>
+     <body>
+		<p1> </p1>
+		<?php
+			$mycounter= 1;
+    		echo "Hello World <br>";
+			echo "My counter $mycounter <br>";
+			echo "Hello friend";
+			
+		?>
+			
+     </body>
+</html>
