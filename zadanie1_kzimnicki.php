3b11f0ade9a01da3002534964b8b27f61bee3bfa
zimsoon <75042481+zimsoon@users.noreply.github.com>
2022-09-25 22:12:18 +0200

    Add files via upload
    Pierwsze zadanie na ocenę

---
+++ zadanie1_kzimnicki.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+
+    <metacharset="utf-8">
+	<title>Zadanie1</title>
+
+</head>
+
+<body>
+
+
+
+    <?php
+	
+    /*for($count =1; $count <= 10; ++$count)
+    {    
+        echo "<br>";
+        for($count1 = 1; $count1 <= 10; ++$count1)
+        {
+            echo "$count * ". $count1 . " = " . $count * $count1 ;
+            echo "<br>";
+        }        
+    }
+    */
+
+    
+        for($count =1; $count <= 6; ++$count)
+    {    
+        echo "<br>";
+
+        for($count1 = 1; $count1 <= 6; ++$count1)
+
+        {
+            echo "*" ;
+        }
+     }   
+
+    ?>
+
+</body>
+</html>
